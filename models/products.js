d67dccfa035ee0920583916d237348faf0b2791d
giovannipc <giovannipaulocunha@hotmail.com>
2019-08-01 11:52:43 -0300

    models ready

---
+++ models/products.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+  {
+    name: String,
+    status: { type: String, enum:['Request', 'FirstReponse', 'ToRepair', 'OrderRepair', 'WantRepair', 'InRepair', 'SendCompany', 'ToStore'], default: 'Request'},
+    statusProdcuct: { type: String, enum:['Broken', 'Semi-used']},
+    categories: { type: String, enum:["Laptop", "Tablet", "Mobile", "Consoles", "TV's", "Audio"]},
+    path: { type: String, enum:['Repair', 'Sell']},
+    brand: String,
+    price: String,
+    description: String,
+    descriptionRepair: String,
+    imageUrl: [{ type: String, default: "http://static.cmsi-id.com/product/01032016/pt-cahayatiara-mustika-scientific-indonesia_5tqwe_244.png"}],
+    owner: { type: Schema.Types.ObjectId, ref: 'User' }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true },
+    password: String,
+    role: { type: String, enum:["Costumer", "Repair", "Admin"], default: "Costumer"},
+    name: String,
+    status: { type: String, enum: ["Active", "Pending"], default: "Pending" },
+    address: String,
+    cpf: String,
+    product: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+    company: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
