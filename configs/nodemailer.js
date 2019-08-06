f08810c2a491fabb39c24ef3d23295b23a5fbbd0
Uira Oliveira <uira.sampaio@hotmail.com>
2019-08-06 16:52:17 -0300

    email setup and other configs

--- configs/nodemailer.js
+++ configs/nodemailer.js
@@ -1,14 +1,25 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp.sparkpostmail.com',
+//   port: 587,
+//   secure: false,
+//   authMethod: 'LOGIN',
+//   auth: {
+//     user: process.env.SPARKPOST_USER,
+//     pass: process.env.SPARKPOST_PASSWORD,
+//   },
+// });
+
+// test usage
+
 const transporter = nodemailer.createTransport({
-  host: 'smtp.sparkpostmail.com',
-  port: 587,
-  secure: false,
-  authMethod: 'LOGIN',
+  host: 'smtp.mailtrap.io',
+  port: 2525,
   auth: {
-    user: process.env.SPARKPOST_USER,
-    pass: process.env.SPARKPOST_PASSWORD,
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
   },
 });
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -34,14 +34,18 @@ authRoutes.post('/signup', (req, res, next) => {
 
     const salt = bcrypt.genSaltSync(10);
     const hashPass = bcrypt.hashSync(password, salt);
-    const confirmationCode = bcrypt.hashSync(username, salt);
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let token = '';
+    for (let i = 0; i < 25; i += 1) {
+      token += characters[Math.floor(Math.random() * characters.length)];
+    }
 
     const newUser = new User({
       username,
       password: hashPass,
       name,
       cpf,
-      token: confirmationCode,
+      token,
       role,
     });
 
@@ -49,17 +53,258 @@ authRoutes.post('/signup', (req, res, next) => {
     newUser.save()
       .then(() => {
         transporter.sendMail({
-          from: '"Phoenix Forge" <phoenixforge@hotmail.com>',
+          from: '"Phoenix Forge" <phixit@gin.ink>',
           to: username,
-          subject: 'Welcome to Phoenix Forge! Please confirm your account.',
-          text: `Please, click on the link below to confirm your account: ${process.env.BASE_URL}/${confirmationCode}`,
-          html: `
-        <h3>Hi, there!</h3>
-        <p>Please, click <a href="${process.env.BASE_URL}/${confirmationCode}" target="_blank">here</a> to confirm your account.</p>`,
+          subject: `Hey,${name}. Welcome to Phix`,
+          text: 'Welcome text',
+          html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+          <html xmlns="http://www.w3.org/1999/xhtml">
+            <head>
+              <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <title>Phix It - Confirm Email</title>
+          
+              <style type="text/css">
+                @import url(http://fonts.googleapis.com/css?family=Droid+Sans);
+          
+                img {
+                  max-width: 600px;
+                  outline: none;
+                  text-decoration: none;
+                  -ms-interpolation-mode: bicubic;
+                }
+          
+                a {
+                  text-decoration: none;
+                  border: 0;
+                  outline: none;
+                  color: #bbbbbb;
+                }
+          
+                a img {
+                  border: none;
+                }
+          
+                td,
+                h1,
+                h2,
+                h3 {
+                  font-family: Helvetica, Arial, sans-serif;
+                  font-weight: 400;
+                }
+          
+                td {
+                  text-align: center;
+                }
+          
+                body {
+                  -webkit-font-smoothing: antialiased;
+                  -webkit-text-size-adjust: none;
+                  width: 100%;
+                  height: 100%;
+                  color: #37302d;
+                  background: #ffffff;
+                  font-size: 16px;
+                }
+          
+                table {
+                  border-collapse: collapse !important;
+                }
+          
+                .headline {
+                  color: #1f1e1b;
+                  font-size: 36px;
+                }
+          
+                .force-full-width {
+                  width: 100% !important;
+                }
+          
+                .force-width-80 {
+                  width: 80% !important;
+                }
+              </style>
+          
+              <style type="text/css" media="screen">
+                @media screen {
+                  /*Thanks Outlook 2013! http://goo.gl/XLxpyl*/
+                  td,
+                  h1,
+                  h2,
+                  h3 {
+                    font-family: "Droid Sans", "Helvetica Neue", "Arial", "sans-serif" !important;
+                  }
+                }
+              </style>
+          
+              <style type="text/css" media="only screen and (max-width: 480px)">
+                /* Mobile styles */
+                @media only screen and (max-width: 480px) {
+                  table[class="w320"] {
+                    width: 320px !important;
+                  }
+          
+                  td[class="mobile-block"] {
+                    width: 100% !important;
+                    display: block !important;
+                  }
+                }
+              </style>
+            </head>
+            <body
+              class="body"
+              style="padding:0; margin:0; display:block; background:#ffffff; -webkit-text-size-adjust:none"
+              bgcolor="#ffffff"
+            >
+              <table
+                align="center"
+                cellpadding="0"
+                cellspacing="0"
+                class="force-full-width"
+                height="100%"
+              >
+                <tr>
+                  <td align="center" valign="top" bgcolor="#ffffff" width="100%">
+                    <center>
+                      <table
+                        style="margin: 0 auto;"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="600"
+                        class="w320"
+                      >
+                        <tr>
+                          <td align="center" valign="top">
+                            <table
+                              style="margin: 0 auto;"
+                              cellpadding="0"
+                              cellspacing="0"
+                              class="force-full-width"
+                              style="margin:0 auto;"
+                            >
+                              <tr>
+                                <td style="font-size: 30px; text-align:center;">
+                                  <br />
+                                  <h2><bold>Phix It - Sell and Repair</bold></h2>
+                                </td>
+                              </tr>
+                            </table>
+          
+                            <table
+                              style="margin: 0 auto;"
+                              cellpadding="0"
+                              cellspacing="0"
+                              class="force-full-width"
+                              bgcolor="#ebd116"
+                            >
+                              <tr>
+                                <td>
+                                  <br />
+                                  <img
+                                    src="https://image.flaticon.com/icons/svg/1792/1792920.svg"
+                                    width="224"
+                                    height="240"
+                                    alt="robot picture"
+                                  />
+                                </td>
+                              </tr>
+                              <tr>
+                                <td class="headline">
+                                  Hey!
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <center>
+                                    <table
+                                      style="margin: 0 auto;"
+                                      cellpadding="0"
+                                      cellspacing="0"
+                                      width="60%"
+                                    >
+                                      <tr>
+                                        <td style="color:#1f1e1b;">
+                                          <br />
+                                          Your account has been created, now you only have
+                                          to confirm the email to have access to login!
+                                          <br />
+                                          <br />
+                                        </td>
+                                      </tr>
+                                    </table>
+                                  </center>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <div>
+                                    <a
+                                      href="${process.env.EMAIL_HANDLER}/api/confirm/${token}"
+                                      style="background-color:#178f8f;border-radius:4px;color:#ffffff;display:inline-block;font-family:Helvetica, Arial, sans-serif;font-size:16px;font-weight:bold;line-height:50px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;"
+                                      >Confirm Here</a
+                                    >
+                                  </div>
+                                  <br />
+                                  <br />
+                                </td>
+                              </tr>
+                            </table>
+          
+                            <table
+                              style="margin: 0 auto;"
+                              cellpadding="0"
+                              cellspacing="0"
+                              class="force-full-width"
+                              bgcolor="#414141"
+                              style="margin: 0 auto"
+                            >
+                              <tr>
+                                <td style="background-color:#414141;">
+                                  <br />
+                                  <br />
+                                  <img
+                                    src="https://www.filepicker.io/api/file/R4VBTe2UQeGdAlM7KDc4"
+                                    alt="google+"
+                                  />
+                                  <img
+                                    src="https://www.filepicker.io/api/file/cvmSPOdlRaWQZnKFnBGt"
+                                    alt="facebook"
+                                  />
+                                  <img
+                                    src="https://www.filepicker.io/api/file/Gvu32apSQDqLMb40pvYe"
+                                    alt="twitter"
+                                  />
+                                  <br />
+                                  <br />
+                                </td>
+                              </tr>
+                              <tr>
+                                <td style="color:#bbbbbb; font-size:12px;">
+                                  <a href="#">View in browser</a> |
+                                  <a href="#">Unsubscribe</a> | <a href="#">Contact</a>
+                                  <br /><br />
+                                </td>
+                              </tr>
+                              <tr>
+                                <td style="color:#bbbbbb; font-size:12px;">
+                                  © 2019 All Rights Reserved
+                                  <br />
+                                  <br />
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </table>
+                    </center>
+                  </td>
+                </tr>
+              </table>
+            </body>
+          </html>`,
         });
 
         req.login(newUser, (err) => {
-
           if (err) {
             res.status(500).json({ message: 'Login after signup went bad.' });
             return;
@@ -67,8 +312,8 @@ authRoutes.post('/signup', (req, res, next) => {
 
           res.status(200).json(newUser);
         });
-
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.log(error);
         res.status(400).json({ message: 'Saving user to database went wrong.' });
       })
@@ -120,11 +365,11 @@ authRoutes.get('/loggedin', (req, res, next) => {
 
 
 authRoutes.get('/confirm/:id', (req, res, next) => {
-  User.findOneAndUpdate({ token: req.params.id }, { $set: { status: 'Active' }})
+  User.findOneAndUpdate({ token: req.params.id }, { $set: { status: 'Active' } })
     .then((answer) => {
       res.status(200).json(answer);
     })
-    .catch(() => res.status(500).json({ message: 'Confirmation failed.' }))      
+    .catch(() => res.status(500).json({ message: 'Confirmation failed.' }))
 })
 
 
